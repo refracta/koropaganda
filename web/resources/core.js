6857738e26f3deddae7ab7260a184f20cc372d4f
refracta <58779799+refracta@users.noreply.github.com>
2021-06-29 09:12:35 +0900

    Add next/execute feature

--- web/resources/core.js
+++ web/resources/core.js
@@ -8,7 +8,8 @@ import Interpreter from "../../interpreter.js";
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
+let clickResolve;
+let executeMode = false;
 let i = new Interpreter(window.alert,
     o => {
         $('#stdout').text($('#stdout').text() + o);
@@ -21,11 +22,21 @@ let i = new Interpreter(window.alert,
         $('#command-line-number').text(`현재 명령 줄 번호: ${e.line}`);
         $('#command-parameter').text(`매개변수: [${e.parameters.filter(e => e !== void 0).join(', ')}]`);
         $('#stack').text(`공기업 스택: [${e.publicCompanyStack.join(', ')}]\n중소기업 스택: [${e.smallCompanyStack.join(', ')}]\n중견기업 스택: [${e.midCompanyStack.join(', ')}]\n대기업 스택: [${e.largeCompanyStack.join(', ')}]`);
-        return new Promise(async r => {
-            await sleep(100);
-            r();
+        return new Promise(async resolve => {
+            clickResolve = resolve;
+            if(executeMode){
+                resolve();
+            }
         })
     });
+$('#next').click(e=>{
+    clickResolve();
+});
+$('#execute').click(e=>{
+    executeMode = true;
+    clickResolve();
+});
+
 $('#start').click(e => {
     i.setStandardInputStack($('#stdin').val());
     $('#stdin').hide();
